12eb0bdee4bc35d385b9c299e1bec57518a33f9a
Diego Argueta <620513-dargueta@users.noreply.github.com>
2019-01-03 14:41:33 -1000

    Fix example to use user data

--- docs/examples/cmos_time/run.lua
+++ docs/examples/cmos_time/run.lua
@@ -1,23 +1,21 @@
 local unicorn = require 'unicorn'
 local x86 = require 'unicorn.x86'
 
-local CMOS_STATE = {}
 
-
-function handle_in(engine, port, size)
+function handle_in(engine, port, size, cmos)
   if port == 0x70 then
     error('Port 0x70 is not readable.')
   elseif port == 0x71 then
-    return handle_71_in(engine, port, size)
+    return handle_71_in(engine, port, size, cmos)
   else
     return 0
   end
 end
 
 
-function handle_out(engine, port, size, value)
+function handle_out(engine, port, size, value, cmos)
   if port == 0x70 then
-    return handle_70_out(engine, port, size, value)
+    return handle_70_out(engine, port, size, value, cmos)
   elseif port == 0x71 then
     error('Writing to port 0x71 is not implemented.')
   else
@@ -26,30 +24,31 @@ function handle_out(engine, port, size, value)
 end
 
 
-function handle_70_out(engine, port, size, value)
+function handle_70_out(engine, port, size, value, cmos)
   if size ~= 1 then
     error('Invalid write size for port 0x70: ' .. tostring(size))
   end
 
-  print(string.format('Writing %#02x to register 0x70', value))
-  CMOS_STATE.selected_register = value
+  print(string.format('Writing %#02x to register %d', value,
+                      cmos.selected_register))
+  cmos.selected_register = value
 end
 
 
-function handle_71_in(engine, port, size)
+function handle_71_in(engine, port, size, cmos)
   if size ~= 1 then
     error('Invalid read size for port 0x71: ' .. tostring(size))
   end
 
-  local reg = CMOS_STATE.selected_register
-  local value = CMOS_STATE.registers[CMOS_STATE.selected_register]
+  local reg = cmos.selected_register
+  local value = cmos.registers[cmos.selected_register]
 
-  print(string.format('Reading register %#02x from port 0x71, got %#02x.',
-                      reg, value))
+  print(string.format('Reading register %d from port 0x71, got %#02x.', reg,
+                      value))
 
-  -- The CMOS usually resets the selected register after a read or write to 13,
+  -- The CMOS usually resets the selected register to 13 after a read or write,
   -- for reasons unknown. Emulate that here.
-  CMOS_STATE.selected_register = 0x0d
+  cmos.selected_register = 0x0d
 
   return value or 0
 end
@@ -62,26 +61,25 @@ _MONTHS = {'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
 
 function main()
   local engine = unicorn.open(unicorn.UC_ARCH_X86, unicorn.UC_MODE_32)
-
-  engine:hook_add(unicorn.UC_HOOK_INSN, handle_in, 0, 0, x86.UC_X86_INS_IN)
-  engine:hook_add(unicorn.UC_HOOK_INSN, handle_out, 0, 0, x86.UC_X86_INS_OUT)
-
-  -- This seems to be the default for some reason
-  CMOS_STATE.selected_register = 0x0d
-
   local now = os.date('*t')
-  CMOS_STATE.registers = {
-    [0] = now.sec,
-    [2] = now.min,
-    [4] = now.hour,
-    [6] = now.wday,
-    [7] = now.day,
-    [8] = now.month,
-    [9] = now.year % 100,
-    [0x32] = math.floor(now.year / 100)
+  local cmos_state = {
+    -- Register 13 seems to be the default for some reason
+    selected_register = 0x0d,
+    registers = {
+      [0] = now.sec,
+      [2] = now.min,
+      [4] = now.hour,
+      [6] = now.wday,
+      [7] = now.day,
+      [8] = now.month,
+      [9] = now.year % 100,
+      [0x32] = math.floor(now.year / 100)
+    }
   }
 
-  engine:mem_map(0, 4096)
+  engine:hook_add(unicorn.UC_HOOK_INSN, handle_in, 0, 0, cmos_state, x86.UC_X86_INS_IN)
+  engine:hook_add(unicorn.UC_HOOK_INSN, handle_out, 0, 0, cmos_state, x86.UC_X86_INS_OUT)
+  engine:mem_map(0, 2^20)
 
   -- Load the program image into memory
   local fdesc = io.open('program.x86.bin', 'rb')
@@ -105,10 +103,9 @@ function main()
 
   engine:close()
 
-  -- FIXME: Month and day are swapped, and those are the only wrong ones.
   print(string.format(
-    '\nToday is: %s, %d %s %d %02d:%02d:%02d (C=%d)', _WEEKDAYS[wday], month,
-    _MONTHS[day], full_year, hour, min, sec, century))
+    '\nToday is: %s, %d %s %d %02d:%02d:%02d (C=%d)', _WEEKDAYS[wday], day,
+    _MONTHS[month], full_year, hour, min, sec, century))
 end
 
 main()
