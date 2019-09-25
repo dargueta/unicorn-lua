9745843ac1d7283a79b5d2c770db4fa8eb291820
Diego Argueta <diego@goodrx.com>
2019-09-25 14:59:37 -0700

    Finish floating-point register handling

--- include/unicornlua/registers.h
+++ include/unicornlua/registers.h
@@ -19,11 +19,6 @@
     #error "Can't handle floating-point radixes other than 2 right now."
 #endif
 
-
-class SoftFloat80;
-class SoftFloat128;
-
-
 #if FLT_MANT_DIG == 24
     typedef float uclua_float32;
 #else
@@ -36,88 +31,74 @@ class SoftFloat128;
     #error "`double` isn't 64 bits. This library can't handle that yet."
 #endif
 
-#if defined(__GNUC__) && defined(__x86_64__) && defined(USE_FLOAT128)
+#if defined(__GNUC__) && defined(__x86__) && defined(USE_FLOAT128)
     #define UCLUA_HAVE_GNUFLOAT128
 #endif
 
+#if defined(LDBL_MANT_DIG)
+    typedef long double uclua_float80;
 
-#ifdef LDBL_MANT_DIG
-    #define UCLUA_HAVE_LONG_DOUBLE
     #if LDBL_MANT_DIG == 64
         // `long double` is 80 bits on this platform.
-        typedef long double uclua_float80;
-
-        // We have no standard way of representing 128-bit floats. GNU C++ provides the
-        // "__float128" type *if* it's enabled on the command line and the target machine
-        // supports it. According to the GCC docs, if the target architecture is x86-64
-        // then __float128 is IEEE-754 binary128 format and we can use it.
-        #if UCLUA_HAVE_GNUFLOAT128
-            #define UCLUA_BIG_FLOAT_SIZE 128
-            typedef __float128 uclua_float128;
-            typedef __float128 uclua_big_float;
-        #else
-            #define UCLUA_BIG_FLOAT_SIZE 80
-            typedef SoftFloat128 uclua_float128;
-            typedef long double uclua_big_float;
-        #endif
+        #define UCLUA_FLOAT80_SIZE 80
     #elif LDBL_MANT_DIG == 113
-        // `long double` is 128 bits on this platform. We need to emulate 80.
-        #define UCLUA_BIG_FLOAT_SIZE 128
-
-        typedef SoftFloat80 uclua_float80;
-        typedef long double uclua_float128;
-        typedef long double uclua_big_float;
-    #endif
-#else
-    // Platform doesn't support `long double` at all.
-    typedef SoftFloat80 uclua_float80;
-
-    #ifdef UCLUA_HAVE_GNUFLOAT128
-        #define UCLUA_BIG_FLOAT_SIZE 128
-        typedef __float128 uclua_float128;
-        typedef __float128 uclua_big_float;
+        // `long double` is 128 bits on this platform.
+        #define UCLUA_FLOAT80_SIZE 128
     #else
-        // No 128-bit floating-point support so we're stuck with `double`
-        #define UCLUA_BIG_FLOAT_SIZE 64
-        typedef double uclua_big_float;
-        typedef SoftFloat128 uclua_float128;
+        // No idea how big a `long double` is but it's biiiig.
+        #define UCLUA_FLOAT80_SIZE 0
     #endif
+#elif defined(UCLUA_HAVE_GNUFLOAT128)
+    // Platform doesn't support `long double` but does support __float128.
+    #define UCLUA_FLOAT80_SIZE 128
+    typedef __float128 uclua_float80;
+#else
+    #error "Platform has no way to represent 80-bit floating-point registers."
 #endif
+
 
 enum RegisterDataType {
     UL_REG_TYPE_UNKNOWN,
-    UL_REG_TYPE_INT8,
-    UL_REG_TYPE_INT16,
-    UL_REG_TYPE_INT32,
-    UL_REG_TYPE_INT64,
+    UL_REG_TYPE_INT8,           // 1 byte
+    UL_REG_TYPE_INT16,          // 2 bytes
+    UL_REG_TYPE_INT32,          // 4 bytes
     UL_REG_TYPE_FLOAT32,
+    UL_REG_TYPE_INT64,          // 8 bytes
     UL_REG_TYPE_FLOAT64,
-    UL_REG_TYPE_FLOAT80,
-    UL_REG_TYPE_FLOAT128,
-
-    // SIMD data types
-
     UL_REG_TYPE_INT8_ARRAY_8,
-    UL_REG_TYPE_INT8_ARRAY_16,
-    // No int8[32] to my knowledge
-    UL_REG_TYPE_INT8_ARRAY_64,
     UL_REG_TYPE_INT16_ARRAY_4,
-    UL_REG_TYPE_INT16_ARRAY_8,
-    // No int16[16]?
     UL_REG_TYPE_INT32_ARRAY_2,
-    UL_REG_TYPE_INT32_ARRAY_4,
-    // No int32[8]?
-    UL_REG_TYPE_INT32_ARRAY_16,
     UL_REG_TYPE_INT64_ARRAY_1,
-    // No int64[2] or int64[4]?
+    UL_REG_TYPE_FLOAT80,            // 10 bytes
+    UL_REG_TYPE_INT8_ARRAY_16,      // 16 bytes
+    UL_REG_TYPE_INT16_ARRAY_8,
+    UL_REG_TYPE_INT32_ARRAY_4,
+    UL_REG_TYPE_INT64_ARRAY_2,
+    UL_REG_TYPE_FLOAT32_ARRAY_4,
+    UL_REG_TYPE_FLOAT64_ARRAY_2,
+    UL_REG_TYPE_INT8_ARRAY_32,      // 32 bytes
+    UL_REG_TYPE_INT16_ARRAY_16,
+    UL_REG_TYPE_INT32_ARRAY_8,
+    UL_REG_TYPE_INT64_ARRAY_4,
+    UL_REG_TYPE_FLOAT32_ARRAY_8,
+    UL_REG_TYPE_FLOAT64_ARRAY_4,
+    UL_REG_TYPE_INT8_ARRAY_64,      // 64 bytes
+    UL_REG_TYPE_INT32_ARRAY_16,
     UL_REG_TYPE_INT64_ARRAY_8,
+    UL_REG_TYPE_FLOAT32_ARRAY_16,
+    UL_REG_TYPE_FLOAT64_ARRAY_8,
 };
 
 
 class Register {
+public:
     Register();
+    Register(const void *buffer, RegisterDataType kind);
 
-    void assign_value(const void *buffer, unsigned n_bytes);
+    void assign_value(const void *buffer, RegisterDataType kind);
+    RegisterDataType get_kind() const noexcept;
+    size_t get_size() const;
+    static size_t size_for_register_kind(RegisterDataType kind);
 
     int8_t as_int8() const noexcept;
     int16_t as_int16() const noexcept;
@@ -127,7 +108,6 @@ class Register {
     uclua_float32 as_float32() const noexcept;
     uclua_float64 as_float64() const noexcept;
     uclua_float80 as_float80() const;
-    uclua_float128 as_float128() const;
 
     // MMX
     std::array<int8_t, 8> as_8xi8() const;
@@ -141,23 +121,29 @@ class Register {
     std::array<int8_t, 16> as_16xi8() const;
     std::array<int16_t, 8> as_8xi16() const;
     std::array<int32_t, 4> as_4xi32() const;
+    std::array<int64_t, 2> as_2xi64() const;
 
     // AVX
     std::array<uclua_float32, 8> as_8xf32() const;
     std::array<uclua_float64, 4> as_4xf64() const;
+    std::array<int8_t, 32> as_32xi8() const;
+    std::array<int16_t, 16> as_16xi16() const;
+    std::array<int32_t, 8> as_8xi32() const;
+    std::array<int64_t, 4> as_4xi64() const;
 
     // AVX-512
+    std::array<uclua_float32, 16> as_16xf32() const;
+    std::array<uclua_float64, 8> as_8xf64() const;
     std::array<int8_t, 64> as_64xi8() const;
     std::array<int16_t, 32> as_32xi16() const;
     std::array<int32_t, 16> as_16xi32() const;
     std::array<int64_t, 8> as_8xi64() const;
-    std::array<uclua_float64, 8> as_8xf64() const;
-    std::array<uclua_float32, 16> as_16xf32() const;
 
 private:
     template <class T, int N> std::array<T, N> array_cast() const;
 
     char data_[64];
+    RegisterDataType kind_;
 };
 
 
@@ -168,5 +154,25 @@ int ul_reg_write(lua_State *L);
 int ul_reg_read(lua_State *L);
 int ul_reg_write_batch(lua_State *L);
 int ul_reg_read_batch(lua_State *L);
+
+
+/**
+ *
+ * @warning There's no way to represent a signaling or "indefinite" NaN in C++. Both of
+ * these values are returned as std::NAN.
+ */
+uclua_float80 read_float80(const uint8_t *data);
+
+
+/**
+ *
+ * @param value     The floating-point value to store as an 80-bit x86 float.
+ * @param buffer    The buffer to store the serialized float into. Must hold at least 10
+ *                  bytes.
+ *
+ * @warning No distinction is made between quiet and signaling NaNs. All NaNs are stored
+ * in memory as a quiet NaN.
+ */
+void write_float80(uclua_float80 value, uint8_t *buffer);
 
 #endif  /* INCLUDE_UNICORNLUA_REGISTERS_H_ */
