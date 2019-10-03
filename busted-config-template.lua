5f84383becaa17320ac0e410e02ca838406119a4
Diego Argueta <diego@goodrx.com>
2019-10-02 18:44:14 -0700

    Start using CMake

--- busted-config-template.lua
+++ busted-config-template.lua
@@ -1,13 +1,14 @@
 return {
   _all = {
-    cpath = "build/?.${LIB_EXTENSION};build/?/init.${LIB_EXTENSION};${LUA_CUSTOM_CPATH}",
-    lpath = "build/?.lua;build/?/init.lua;${LUA_CUSTOM_LPATH}",
-    lua = "${LUA_EXE}",
+    cpath = "@CMAKE_CURRENT_BINARY_DIR@/?@CMAKE_SHARED_LIBRARY_SUFFIX@;@CMAKE_CURRENT_BINARY_DIR@/?/init@CMAKE_SHARED_LIBRARY_SUFFIX@;@LUA_CUSTOM_CPATH@",
+    lpath = "@CMAKE_CURRENT_BINARY_DIR@/?.lua;@CMAKE_CURRENT_BINARY_DIR@/?/init.lua;@LUA_CUSTOM_LPATH@",
+    lua = "@LUA_EXE@",
     verbose = true,
     shuffle = true,
+    directory = "@CMAKE_CURRENT_SOURCE_DIR@",
   },
   default = {
     pattern = "lua",
-    ROOT = {"./tests/lua"},
+    ROOT = {"@CMAKE_CURRENT_SOURCE_DIR@/tests/lua"},
   }
 }
