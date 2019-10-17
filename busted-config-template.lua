f0a6b83cf2234f50bdc5d7184a3f87108523dec4
Diego Argueta <620513-dargueta@users.noreply.github.com>
2019-10-17 09:31:33 -0700

    Fix file path issues

--- busted-config-template.lua
+++ busted-config-template.lua
@@ -1,7 +1,6 @@
 return {
   _all = {
-    cpath = "@BUILT_LIBRARY_SEARCH_DIR@/?@CMAKE_SHARED_LIBRARY_SUFFIX@;@LUA_CUSTOM_CPATH@",
-    lpath = "@CMAKE_CURRENT_BINARY_DIR@/?.lua;@CMAKE_CURRENT_BINARY_DIR@/?/init.lua;@LUA_CUSTOM_LPATH@",
+    cpath = "@BUILT_LIBRARY_DIRECTORY@/?@CMAKE_SHARED_LIBRARY_SUFFIX@;;",
     lua = "@LUA_EXE@",
     verbose = true,
     shuffle = true,
