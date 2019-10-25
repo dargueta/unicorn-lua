6283d772f89931d4f48c3d3c290b677dbcc20eb9
Diego Argueta <diego@goodrx.com>
2019-10-25 11:56:09 -0700

    Compensate for changes to Chocolatey

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -4,13 +4,13 @@ if [[ $TRAVIS_OS_NAME = 'windows' ]]; then
     # Install pre-built binaries on Windows
     wget --tries=3 https://github.com/unicorn-engine/unicorn/releases/download/${UNICORN_VERSION}/unicorn-${UNICORN_VERSION}-win64.zip
     unzip -b unicorn-${UNICORN_VERSION}-win64.zip
-    sudo mv unicorn-${UNICORN_VERSION}-win64/*.dll /c/Windows
+    mv unicorn-${UNICORN_VERSION}-win64/*.dll /c/Windows
 else
-  # Build from source on OSX and Linux
-  wget --tries=3 https://github.com/unicorn-engine/unicorn/archive/${UNICORN_VERSION}.zip
-  unzip -b ${UNICORN_VERSION}.zip
-  pushd unicorn-${UNICORN_VERSION}
-  ./make.sh
-  sudo ./make.sh install
-  popd
+    # Build from source on OSX and Linux
+    wget --tries=3 https://github.com/unicorn-engine/unicorn/archive/${UNICORN_VERSION}.zip
+    unzip -b ${UNICORN_VERSION}.zip
+    pushd unicorn-${UNICORN_VERSION}
+    ./make.sh
+    sudo ./make.sh install
+    popd
 fi
--- ci/travis-setup.sh
+++ ci/travis-setup.sh
@@ -1,23 +1,12 @@
-#!/bin/bash
-
-if [[ $TRAVIS_OS_NAME = 'windows' ]]; then
-    choco install python python2 make cmake
-
-    # Because Python 2 and 3 are installed as `python.exe` Windows can only run one of
-    # them at a time. Python 3 needs to be first on the search path, and can only be
-    # referred to as `python` not `python3`. It also can't find `pip` so we have to use
-    # the indirect way of running it.
-    export PATH="/c/Python37:/c/Python37/Scripts:$PATH"
-    python -m pip install -U -r python-requirements.txt
-fi
-
 # NOTE: This will break the builds when Travis changes the version(s) of Python they
 # have installed.
 if [[ $TRAVIS_OS_NAME = 'linux' ]]; then
     pyenv local 2.7.15 3.7.1
-    pip3 install -U -r python-requirements.txt
 fi
 
-if [[ $TRAVIS_OS_NAME = 'osx' ]]; then
+if [[ $TRAVIS_OS_NAME = 'windows' ]]; then
+    choco install -y python3 python2 make cmake
+    /c/Python38/Python -m pip install -U -r python-requirements.txt
+else
     pip3 install -U -r python-requirements.txt
 fi
