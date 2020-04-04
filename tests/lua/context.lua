56e349f13ba8b8bf86acfed5dd9742d7366a0675
Diego Argueta <620513-dargueta@users.noreply.github.com>
2020-04-04 16:43:03 -0700

    Start more tests

---
+++ tests/lua/context.lua
@@ -0,0 +1,23 @@
+local unicorn = require 'unicorn'
+local uc_const = require 'unicorn.unicorn_const'
+local x86 = require 'unicorn.x86_const'
+
+
+describe('Context tests', function ()
+    it('Basic test of set/restore context', function ()
+        -- Set EAX to a value, save a context, change EAX, restore the context,
+        -- and ensure it has the original value.
+        local uc = unicorn.open(uc_const.UC_ARCH_X86, uc_const.UC_MODE_32)
+
+        uc:reg_write(x86.UC_X86_REG_EAX, 123456)
+        assert.are.equals(123456, uc:reg_read(x86.UC_X86_REG_EAX))
+
+        local context = uc:context_save()
+
+        uc:reg_write(x86.UC_X86_REG_EAX, 98765432)
+        assert.are.equals(98765432, uc:reg_read(x86.UC_X86_REG_EAX))
+
+        uc:context_restore(context)
+        assert.are.equals(123456, uc:reg_read(x86.UC_X86_REG_EAX))
+      end)
+end)
