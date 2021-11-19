b35bbff88db50d1f3a7138e1d27400767b77eb8a
Diego Argueta <620513-dargueta@users.noreply.github.com>
2021-11-19 08:13:41 -0800

    Potentially fix data installation directory on Windows

--- tools/ci/install-unicorn.py
+++ tools/ci/install-unicorn.py
@@ -1,7 +1,6 @@
 import ctypes
 import logging
 import os
-import shlex
 import shutil
 import subprocess
 import sys
@@ -20,6 +19,7 @@ sudo ./make.sh install
 """
 
 UNICORN_VERSION = sys.argv[1]
+ARCHIVE_DIRECTORY = "unicorn-{version}-win{bits}"
 WINDOWS_URL_TEMPLATE = "https://github.com/unicorn-engine/unicorn/releases/download/{version}/unicorn-{version}-win{bits}.zip"
 
 
@@ -34,11 +34,26 @@ def install_unixlike():
 
 
 def get_windows_system_install_dir():
+    """Get the directory where we should install the Unicorn DLL.
+
+    See: https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/nf-sysinfoapi-getsystemdirectorya
+    """
     buf = ctypes.create_string_buffer(b"\x00" * 256)
     ctypes.windll.kernel32.GetSystemDirectoryA(buf, 256)
     directory = buf.value.decode("ascii")
     logging.info("Found Windows DLL installation directory: %r", directory)
     return directory
+
+
+def get_app_data_dir():
+    root = os.getenv("LOCALAPPDATA")
+    if root is None:
+        root = tempfile.gettempdir()
+        logging.error(
+            "LOCALAPPDATA environment variable is empty, falling back to %r", root
+        )
+    root = os.path.abspath(root)
+    return os.path.join(root, "UnicornEngine")
 
 
 def install_windows():
@@ -49,6 +64,8 @@ def install_windows():
         bits = 32
 
     url = WINDOWS_URL_TEMPLATE.format(version=UNICORN_VERSION, bits=bits)
+
+    logging.info("Downloading Unicorn archive from: %s", url)
     response = urllib.request.urlopen(url)
     if response.status != 200:
         logging.error(
@@ -60,19 +77,35 @@ def install_windows():
         return sys.exit(1)
 
     with tempfile.TemporaryDirectory() as tmp_dir:
+        logging.info("Copying archive to temporary directory: %s", tmp_dir)
         archive_file_path = os.path.join(tmp_dir, "__archive.zip")
         with open(archive_file_path, "wb") as fd:
             shutil.copyfileobj(response, fd, 2 ** 20)
 
         # Unpack the zip archive into a vaguely reasonable directory. This contains the
         # DLL and header files.
-        os.makedirs("C:\\Program Files\\UnicornEngine")
-        shutil.unpack_archive(archive_file_path, "C:\\Program Files\\UnicornEngine")
+        data_dir = get_app_data_dir()
+
+        logging.info("Creating installation directory: %s", data_dir)
+        os.makedirs(data_dir)
+
+        logging.info(
+            "Unpacking archive to data directory: %s -> %s", archive_file_path, data_dir
+        )
+        shutil.unpack_archive(archive_file_path, data_dir)
+
+        logging.info("(DEBUG) TMPDIR tree at %s: %s", data_dir, os.listdir(data_dir))
 
         # Put the DLL where Windows can find it
+        dll_dir = get_windows_system_install_dir()
+        logging.info("Moving DLL to Windows directory: %s", dll_dir)
         shutil.move(
-            "C:\\Program Files\\UnicornEngine\\unicorn.dll",
-            get_windows_system_install_dir(),
+            os.path.join(
+                data_dir,
+                ARCHIVE_DIRECTORY.format(version=UNICORN_VERSION, bits=bits),
+                "unicorn.dll",
+            ),
+            dll_dir,
         )
 
 
