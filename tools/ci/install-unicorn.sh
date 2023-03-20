5012ba344b4409ab3d7555ce73b725db7017f0e7
Diego Argueta <dargueta@users.noreply.github.com>
2023-03-20 10:50:32 -0700

    Handle 2.x installation process

--- tools/ci/install-unicorn.sh
+++ tools/ci/install-unicorn.sh
@@ -8,5 +8,15 @@ git clone --depth 1 https://github.com/unicorn-engine/unicorn.git unicorn-${UNIC
 cd unicorn-${UNICORN_VERSION}
 git fetch --all --tags --prune
 git checkout ${UNICORN_VERSION}
-./make.sh
-sudo ./make.sh install
+
+# 2.x has a different installation process than 1.x
+if echo "$UNICORN_VERSION" | grep '^2\.' 1>/dev/null; then
+  # Unicorn 2.x
+  mkdir build
+  cd build
+  cmake .. -DCMAKE_BUILD_TYPE=Release
+  sudo make install
+else
+  ./make.sh
+  sudo ./make.sh install
+fi
