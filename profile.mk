e94a2f77e9c4f87400814e59219b8323cf46b231
Diego Argueta <dargueta@users.noreply.github.com>
2023-03-22 10:01:24 -0700

    Shove stuff into the Makefile template

--- profile.mk
+++ profile.mk
@@ -28,9 +28,6 @@ all: lua-profile.mk lua-profile.cmake
 
 lua-profile.mk: tools/profile_lua.lua
 	$(PROFILE) -f make $@
-	echo "\nBUILD_TYPE := $(BUILD_TYPE)" >> $@
-	echo "BUILD_DIR := $(BUILD_DIR)" >> $@
-	echo "REPO_ROOT := `pwd`" >> $@
 
 lua-profile.cmake: tools/profile_lua.lua
 	$(PROFILE) -f cmake $@
