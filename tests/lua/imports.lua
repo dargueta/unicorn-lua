b78af27e6edbd2c482ea4eb37f847726353cc890
Diego Argueta <dargueta@users.noreply.github.com>
2023-04-17 10:09:27 -0700

    Test 2.x architecture imports

--- tests/lua/imports.lua
+++ tests/lua/imports.lua
@@ -7,6 +7,20 @@ describe("Ensure library loads don't crash.", function ()
   it('[mips] require', function () require 'unicorn.mips_const' end)
   it('[sparc] require', function () require 'unicorn.sparc_const' end)
   it('[x86] require', function () require 'unicorn.x86_const' end)
+
+  -- Unicorn 2.x only
+  describe("Unicorn 2.x tests only", function ()
+    for _, arch in ipairs({"ppc", "riscv", "s390x", "tricore"}) do
+      it("[" .. arch .. "] require", function()
+        local uc = require "unicorn"
+        local v_major = uc:version()
+
+        if v_major >= 2 then
+          require("unicorn." .. arch .. "_const")
+        end
+      end)
+    end
+  end)
 end)
 
 
