d13d74dcf2cc986f73ee828df3b1733efdb1f1b7
Diego Argueta <dargueta@users.noreply.github.com>
2024-10-03 05:35:59 -0700

    Add copyright notice to generated Lua files

--- tools/render_template.lua
+++ tools/render_template.lua
@@ -21,6 +21,25 @@ template = require "pl.template"
 utils = require "pl.utils"
 
 
+COPYRIGHT_NOTICE = [[
+Copyright (C) 2017-2024 by Diego Argueta
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+]]
+
+
 USAGE = [[
 Render a template.
 
@@ -62,6 +81,8 @@ Arguments:
 
 
 function main()
+    stringx.import()
+
     local args = lapp(USAGE)
     local environment = {
         ipairs = ipairs,
@@ -69,6 +90,7 @@ function main()
         string = string,
         stringx = stringx,
         tablex = tablex,
+        copyright_notice = COPYRIGHT_NOTICE:splitlines(),
     }
 
     for _, file_path in ipairs(args.value_files) do
