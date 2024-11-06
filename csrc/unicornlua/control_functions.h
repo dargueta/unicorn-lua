afa3f8de9ca71c292a1544283b9d41da1cd5e567
Diego Argueta <dargueta@users.noreply.github.com>
2024-11-06 15:18:44 -0800

    Fix changed function name for Unicorn 1.x unsupported exception function

--- csrc/unicornlua/control_functions.h
+++ csrc/unicornlua/control_functions.h
@@ -37,19 +37,19 @@ int ul_ctl_set_cpu_model(lua_State *L);
 int ul_ctl_set_exits(lua_State *L);
 int ul_ctl_set_page_size(lua_State *L);
 #else
-#    define ul_ctl_exits_disable ul_crash_unsupported_operation
-#    define ul_ctl_exits_enable ul_crash_unsupported_operation
-#    define ul_ctl_flush_tlb ul_crash_unsupported_operation
-#    define ul_ctl_get_arch ul_crash_unsupported_operation
-#    define ul_ctl_get_cpu_model ul_crash_unsupported_operation
-#    define ul_ctl_get_exits ul_crash_unsupported_operation
-#    define ul_ctl_get_exits_cnt ul_crash_unsupported_operation
-#    define ul_ctl_get_mode ul_crash_unsupported_operation
-#    define ul_ctl_get_page_size ul_crash_unsupported_operation
-#    define ul_ctl_get_timeout ul_crash_unsupported_operation
-#    define ul_ctl_remove_cache ul_crash_unsupported_operation
-#    define ul_ctl_request_cache ul_crash_unsupported_operation
-#    define ul_ctl_set_cpu_model ul_crash_unsupported_operation
-#    define ul_ctl_set_exits ul_crash_unsupported_operation
-#    define ul_ctl_set_page_size ul_crash_unsupported_operation
+#    define ul_ctl_exits_disable ulinternal_crash_unsupported_operation
+#    define ul_ctl_exits_enable ulinternal_crash_unsupported_operation
+#    define ul_ctl_flush_tlb ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_arch ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_cpu_model ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_exits ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_exits_cnt ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_mode ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_page_size ulinternal_crash_unsupported_operation
+#    define ul_ctl_get_timeout ulinternal_crash_unsupported_operation
+#    define ul_ctl_remove_cache ulinternal_crash_unsupported_operation
+#    define ul_ctl_request_cache ulinternal_crash_unsupported_operation
+#    define ul_ctl_set_cpu_model ulinternal_crash_unsupported_operation
+#    define ul_ctl_set_exits ulinternal_crash_unsupported_operation
+#    define ul_ctl_set_page_size ulinternal_crash_unsupported_operation
 #endif
