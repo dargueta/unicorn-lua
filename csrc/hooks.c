f23c473374443e1ec99c060623d115686af2b578
Diego Argueta <dargueta@users.noreply.github.com>
2024-11-07 07:05:43 -0800

    Fix some compilation errors, better error message

--- csrc/hooks.c
+++ csrc/hooks.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <lauxlib.h>
 #include <lua.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unicorn/unicorn.h>
@@ -94,15 +95,30 @@ int ul_create_generic_hook_with_no_arguments(lua_State *L)
 {
     ULHook *hook = get_common_arguments(L);
 
+    /* ISO C forbids casting a function pointer to an object pointer (void* in this case).
+     * As Unicorn requires us to do this, we have to disable pedantic warnings temporarily
+     * so that the compiler doesn't blow up. */
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
     uc_err error = uc_hook_add(hook->engine, &hook->hook_handle, hook->hook_type,
                                (void *)ulinternal_hook_callback__no_arguments, hook,
                                hook->start_address, hook->end_address);
+#pragma GCC diagnostic pop
+
     if (error != UC_ERR_OK)
     {
+        /* Because we need to free `hook` to avoid a memory leak, we need to copy out
+         * arguments we're going to use in the error message. Annoying, but necessary. */
         int hook_type = (int)hook->hook_type;
+        uint64_t start = hook->start_address;
+        uint64_t end = hook->end_address;
         free(hook);
-        ulinternal_crash_if_failed(L, error, "Failed to create generic hook of type %d",
-                                   hook_type);
+
+        ulinternal_crash_if_failed(
+            L, error,
+            "Failed to create generic hook of type %d from address 0x%08" PRIX64
+            " through 0x%08" PRIX64 " (start > end means \"all of memory\")",
+            hook_type, start, end);
     }
 
     lua_pushlightuserdata(L, hook);
@@ -118,10 +134,14 @@ int ul_hook_del(lua_State *L)
 {
     ULHook *hook = (ULHook *)lua_topointer(L, 1);
     uc_err error = uc_hook_del(hook->engine, hook->hook_handle);
+
+    /* We're deliberately not deallocating `hook` yet. If this function fails inside a
+     * protected call, the user may want to try again later. */
     ulinternal_crash_if_failed(L, error, "Failed to unset hook.");
 
     luaL_unref(L, LUA_REGISTRYINDEX, hook->extra_data_ref);
     luaL_unref(L, LUA_REGISTRYINDEX, hook->callback_ref);
+    hook->engine = NULL;
     free(hook);
     return 0;
 }
