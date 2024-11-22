52a124597464c5316b31b3966e43796bab0674d3
Diego Argueta <dargueta@users.noreply.github.com>
2024-11-21 20:56:34 -0800

    Simplify templated hook definitions

--- csrc/template_data/basic_hook_functions.lua
+++ csrc/template_data/basic_hook_functions.lua
@@ -1,32 +1,18 @@
 basic_hook_functions = {
     code = {
-        arguments = {
-            { name = "address", type = "uint64_t" },
-            { name = "size", type = "uint32_t" },
-        },
+        arguments = { "uint64_t", "uint32_t" },
     },
 
     port_out = {
-        arguments = {
-            { name = "port", type = "uint32_t" },
-            { name = "size", type = "int" },
-            { name = "value", type = "uint32_t" },
-        },
+        arguments = { "uint32_t", "int", "uint32_t" },
     },
 
     memory_access = {
-        arguments = {
-            { name = "type", type = "uc_mem_type" },
-            { name = "address", type = "uint64_t" },
-            { name = "size", type = "int" },
-            { name = "value", type = "int64_t" },
-        },
+        arguments = { "uc_mem_type", "uint64_t", "int", "int64_t" },
     },
 
     interrupt = {
-        arguments = {
-            { name = "intno", type = "uint32_t" },
-        }
+        arguments = { "uint32_t" }
     },
 
     generic_no_arguments = {
