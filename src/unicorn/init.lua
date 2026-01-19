2acc3447310f69ca20d37c87ef99acfbbbfa2470
Diego Argueta <dargueta@users.noreply.github.com>
2026-01-19 08:05:23 -0800

    Note `mode` defaults to 0 in `unicorn.open()`

--- src/unicorn/init.lua
+++ src/unicorn/init.lua
@@ -65,10 +65,10 @@ local M = {
 ---
 --- @tparam int architecture  An enum value indicating which architecture to emulate. The
 --- constants are available in @{unicorn_const} and all start with `UC_ARCH_`.
---- @tparam int mode_flags  Flags providing broad details of the CPU's operating mode.
---- For example, these can be used to choose between starting an x86 CPU in 16-, 32-, or
---- 64-bit mode, or selecting the endianness on bi-endian architectures. These flags are
---- available in @{unicorn_const} and all start with `UC_MODE_`.
+--- @tparam[opt=0] int mode_flags  Flags providing broad details of the CPU's operating.
+--- mode. For example, these can be used to choose between starting an x86 CPU in 16-,
+--- 32-, or 64-bit mode, or selecting the endianness on bi-endian architectures. These
+--- flags are available in @{unicorn_const} and all start with `UC_MODE_`.
 ---
 --- @treturn Engine  An initialized @{engine.Engine}.
 ---
